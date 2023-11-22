915e40fdfe815dea4133792d89712b315229e820
Teitei Wisdom <120546016+Ajinormotor@users.noreply.github.com>
2023-11-22 09:28:49 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//var hero_content = document.getElementById("hero_content");
+
+//hero_content.onclick() => {
+    //hero_content.style.top="0px";
+//}
+
+function showIcon() {
+    var heroContent = document.getElementById("hero_content");
+    if (heroContent) {
+        heroContent.style.top = "0px";
+    }
+}
