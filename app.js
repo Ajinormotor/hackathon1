8f73d4ec2a5f2c40d9e16ffa7c565126fcc88e2e
Teitei Wisdom <teiteiwisdom@gmail.com>
2023-11-22 22:59:52 -0800

    Making chnages to it

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function show() {
+
+  var content = document.getElementById("content");
+  if(content) {
+    content.style.position  = "unset"; 
+  console.log("it has been positioned")
+
+  } 
+}
+
